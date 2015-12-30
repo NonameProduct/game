e5438ffeda287a4325cc691e44cf2c8fcf5cab5d
Till <itsmytrash@web.de>
2015-12-30 22:22:40 +0100

    Enabled the jgrapht library. For some reason, I had to use a downloaded JAR, I did not succeed in having gradle download it on its own.

---
+++ app/src/main/java/com/example/till/game/CompoundIsland.java
@@ -0,0 +1,72 @@
+package com.example.till.game;
+
+import android.view.MotionEvent;
+
+import org.jgrapht.graph.SimpleGraph;
+
+/**
+ * Created by till on 30.12.15.
+ */
+public class CompoundIsland implements Dockable {
+    private SimpleGraph<Triangle, Double> g;
+
+    @Override
+    public double[] getPositionInParent() {
+        return new double[0];
+    }
+
+    @Override
+    public double[] getMovement() {
+        return new double[0];
+    }
+
+    @Override
+    public double getCurrentRotation() {
+        return 0;
+    }
+
+    @Override
+    public double getRotationSpeed() {
+        return 0;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void rollbackUpdate() {
+
+    }
+
+    @Override
+    public Dockable focus() {
+        return null;
+    }
+
+    @Override
+    public void unfocus() {
+
+    }
+
+    @Override
+    public boolean isFocused() {
+        return false;
+    }
+
+    @Override
+    public boolean handleFling(double event1x, double event1y, double event2x, double event2y, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public boolean isInside(double x, double y) {
+        return false;
+    }
+
+    @Override
+    public void handleTap(MotionEvent event) {
+
+    }
+}
